750a029273d743d5dae28f8b13edb0129061ed9a
abiv23 <abiv23@gmail.com>
2025-03-15 15:50:25 -0600

    seo page added circle background component

---
+++ src/app/seo/page.js
@@ -0,0 +1,11 @@
+// src/app/seo/page.js
+import { BackgroundCircles } from '../../components/BackgroundCircles';
+
+export default function SEOPage() {
+  return (
+    <div className="relative min-h-screen">
+      {/* Background element */}
+      <BackgroundCircles />
+    </div>
+  );
+}
